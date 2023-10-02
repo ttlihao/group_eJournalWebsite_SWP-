28b9feaba6f74108dac368ae2aa52d61407464df
ttlihao <thienphan20122003@gmail.com>
2023-10-02 15:49:02 +0700

    new
    added a web layout

---
+++ src/java/com/group4/ejournal/dbutility/DBUtil.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.group4.ejournal.dbutility;
+
+/**
+ *
+ * @author Phan Thien
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+    public static final String USER_NAME = "sa";
+    public static final String PASSWORD = "12345";
+    public static final String DB_NAME = "SWP391";
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://localhost:1433;databaseName= " + DB_NAME;
+        conn = DriverManager.getConnection(url, USER_NAME, PASSWORD);
+        return conn;
+    }
+}
